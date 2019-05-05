17966f04b4631b4ab7998891f60a052a274788e2
skely <skely@volny.cz>
2019-05-05 14:07:03 +0200

    doplneni zadani

--- uloha1.py
+++ uloha1.py
@@ -8,4 +8,7 @@ if __name__ == '__main__':
     for i in range(20):
         matice = np.array(range(100))
         plt.plot(funkce((matice*0.1)*(i))+i)
+
+    # pointa: plot umi pracovat se vstupem jako je matice, matice musi byt ve spravnem tvaru. 
+    # plt.plot(Y)
     plt.show()
