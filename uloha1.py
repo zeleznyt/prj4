1ff89a595f2b117a6bc5b97b0b8bcd700e4aadd5
Tomáš Železný <tomaszelezo@seznam.cz>
2019-05-05 15:26:11 +0200

    úprava podle zadání

--- uloha1.py
+++ uloha1.py
@@ -5,10 +5,12 @@ def funkce(x):
     return np.sin(x)
 
 if __name__ == '__main__':
+    Y = np.array([np.zeros(100)])
+  
     for i in range(20):
-        matice = np.array(range(100))
-        plt.plot(funkce((matice*0.1)*(i))+i)
+        vektor = funkce((np.array(range(100))*0.1)*(i))+i
+        Y =(np.append(Y, [vektor], axis=0))
 
     # pointa: plot umi pracovat se vstupem jako je matice, matice musi byt ve spravnem tvaru. 
-    # plt.plot(Y)
+    plt.plot(Y.transpose())
     plt.show()
