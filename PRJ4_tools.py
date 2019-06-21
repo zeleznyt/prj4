13a6f9a3cc99fac0042ff9a07aa416c5db9f9107
Rumdžiga Nesermidookna <lordtroll800@gmail.com>
2019-06-21 15:19:10 +0200

    sign_synthesis_linear complete

--- PRJ4_tools.py
+++ PRJ4_tools.py
@@ -10,5 +10,8 @@ def sign_synthesis_linear(_sign_1, _sign_2, _gap_length):
     :return: resulting_trajectory framesR X markers (dim = frames1+frames2+_gap_length X markers)
     """
     inter = np.zeros((_gap_length, np.size(_sign_1, 1)))
+    for traj in range(np.size(_sign_1, 1)):
+        for frame in range(_gap_length):
+            inter[frame, traj] = -1*(_sign_1[0,traj]-_sign_2[0,traj])/_gap_length*frame+_sign_1[0,traj]
     res = np.concatenate((_sign_1, inter, _sign_2), axis=0)
     return res
