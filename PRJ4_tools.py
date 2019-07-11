2ad8959173f7c19ff33c4d63e328ac4c4f36cee4
Rumdžiga Nesermidookna <lordtroll800@gmail.com>
2019-07-11 13:21:28 +0200

    Verze 1
    linear, kubic working
    velocity, acceleration bad

--- PRJ4_tools.py
+++ PRJ4_tools.py
@@ -1,17 +1,66 @@
 import numpy as np
 
 
-def sign_synthesis_linear(_sign_1, _sign_2, _gap_length):
+def sign_synthesis(_sign_1, _sign_2, _gap_length, _type):
     """
     Synthesizes linear interpolation of movement
     :param _sign_1: trajectory frames1 X markers
     :param _sign_2: trajectory frames2 X markers
     :param _gap_length: number of frames (int)
+    :param _type: type of interpolation: 'linear', 'kubic'
     :return: resulting_trajectory framesR X markers (dim = frames1+frames2+_gap_length X markers)
     """
-    inter = np.zeros((_gap_length, np.size(_sign_1, 1)))
-    for traj in range(np.size(_sign_1, 1)):
-        for frame in range(_gap_length):
-            inter[frame, traj] = -1*(_sign_1[0,traj]-_sign_2[0,traj])/_gap_length*frame+_sign_1[0,traj]
-    res = np.concatenate((_sign_1, inter, _sign_2), axis=0)
+    if _type == 'linear':
+        _sign_1_t = _sign_1[-1:,:]
+        _sign_2_t = _sign_2[0:1,:]
+        inter = np.zeros((_gap_length, np.size(_sign_1_t, 1)))
+        for traj in range(np.size(_sign_1_t, 1)):
+            for frame in range(_gap_length):
+                inter[frame, traj] = -1*(_sign_1_t[0,traj]-_sign_2_t[0,traj])/_gap_length*frame+_sign_1_t[0,traj]
+        res = inter
+    elif _type == 'kubic':
+        if np.size(_sign_1,0) < 2:
+            res = -1
+        else:
+            _sign_1_t = _sign_1[-2:,:]
+            _sign_2_t = _sign_2[0:2,:]
+            inter = np.zeros((_gap_length, np.size(_sign_1_t, 1)))
+            for traj in range(np.size(_sign_1_t, 1)):
+                y1 = _sign_1_t[1,traj]
+                y2 = _sign_2_t[0,traj]
+                k1 = _sign_1_t[0,traj]-_sign_1_t[1,traj]
+                k2 = _sign_2_t[1,traj]-_sign_2_t[0,traj]
+                for frame in range(_gap_length):
+                    t = (frame)/(_gap_length)
+                    a = k1*_gap_length - (y2-y1)
+                    b = -k2*_gap_length + (y2-y1)
+                    inter[frame, traj] = (1-t)*y1 + t*y2 + t*(1-t)*((1-t)*a + t*b)
+            res = inter
+    else:
+        res = -1
     return res
+
+def sign_velocity_acceleration(_sign):
+    """
+    Calculates max velocity and acceleration in the sign
+    :param _sign: trajectory frames X markers
+    :return: max_velocity, max_acceleration, arg_max_vel, arg_max_acc (type = float, float, int, int)
+    Uses the right difference: v(t) = x(t) - x(t+1)
+    """
+    print(np.shape(_sign))
+    if np.size(_sign, 0) < 3:
+        return -1
+    velocity = np.zeros((np.size(_sign, 0)-1, np.size(_sign, 1)))
+    acceleration = np.zeros((np.size(_sign, 0)-2, np.size(_sign, 1)))
+    print(np.shape(acceleration))
+    for v in range(np.size(velocity, 0)):
+        for traj in range(int(np.size(velocity, 1)/3)):
+            velocity[v, traj] = np.sqrt(np.power(_sign[v, 3*traj]-_sign[v+1, 3*traj], 2)+np.power(_sign[v, 3*traj+1]-_sign[v+1, 3*traj+1], 2)+np.power(_sign[v, 3*traj+2]-_sign[v+1, 3*traj+2], 2))
+            
+    for a in range(np.size(acceleration, 0)):
+        for traj in range(np.size(acceleration, 1)):
+            acceleration[a, traj] = (velocity[a, traj]-velocity[a+1, traj])
+    
+    print(velocity[:10,:1])
+    print(acceleration[:10,:1])
+    return velocity, acceleration
--- run_v1.py
+++ run_v1.py
@@ -1,10 +1,12 @@
+import os
 import PRJ4_tools
 import TRC_tools
 import numpy as np
 import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
-    TRC_infile = 'projevy_pocasi_02.TRC'
+    path_to_data = 'C:/Users/Tomas/Documents/Škola/FAV/PRJ/PRJ4/Data'
+    TRC_infile = os.path.join(path_to_data, 'projevy_pocasi_02.TRC')
     # slovnik_infile = 'pocasi_slovnik9.txt'
     trajectory_matrix, metadata = TRC_tools.TRC_load(TRC_infile)
     # slovnik = TRC_tools.dict_read(slovnik_infile)
@@ -15,23 +17,43 @@ if __name__ == '__main__':
     #     if file_name_pattern in tmp_item['bvh_source']:
     #         print(tmp_item)
     #
+
     znak_1_source_trajectory = trajectory_matrix
-    znak_1_annotation = 2959
-    znak_1_history_length = 1
+    znak_1_annotation = 2967
+    znak_1_history_length = 2
     znak_1 = znak_1_source_trajectory[znak_1_annotation-znak_1_history_length:znak_1_annotation, :]
 
     znak_2_source_trajectory = trajectory_matrix
     znak_2_annotation = 3009
-    znak_2_future_length = 1
+    znak_2_future_length = 2
     znak_2 = znak_2_source_trajectory[znak_2_annotation:znak_2_annotation+znak_2_future_length, :]
 
     int_len = znak_2_annotation-znak_1_annotation
-    res_traj = PRJ4_tools.sign_synthesis_linear(znak_1, znak_2, int_len)
-
-    print(len(res_traj))
-    print(znak_2_annotation- znak_1_annotation)
-
-    plt.plot(res_traj[:, :3], 'b', label='orig')
-    plt.plot(trajectory_matrix[znak_1_annotation-znak_1_history_length:znak_2_annotation+znak_2_future_length, :3], 'g', label='synth')
-    plt.legend()
+    res_traj = PRJ4_tools.sign_synthesis(znak_1, znak_2, int_len, 'kubic')
+    
+    
+    _,axs = plt.subplots(3)
+    v,a = PRJ4_tools.sign_velocity_acceleration(res_traj)
+    axs[0].plot(a[:,:1])
+    axs[1].plot(v[:,:1])
+    axs[2].plot(res_traj[:,:3])
     plt.show()
+
+    _,axs = plt.subplots(3)
+    axs[0].plot(res_traj[:,:1])
+    axs[1].plot(res_traj[:,1:2])
+    axs[2].plot(res_traj[:,2:3])
+    plt.show()
+
+    print(np.shape(znak_1))
+    new_TRC = np.concatenate([znak_1_source_trajectory[znak_1_annotation-30:znak_1_annotation], res_traj, znak_2_source_trajectory[znak_2_annotation:znak_2_annotation+30]], axis = 0)
+
+    plt.plot(new_TRC[:,:3])
+    plt.plot(znak_1_source_trajectory[znak_1_annotation-30:znak_2_annotation+30,:3])
+    # plt.show()
+
+    # TRC_tools.show_pose_comparison(znak_1[-1,:], znak_2[0,:])
+    # plt.plot(res_traj[:, :3], 'b', label='orig')
+    # plt.plot(trajectory_matrix[znak_1_annotation:znak_2_annotation, :3], 'g', label='synth')
+    # plt.legend()
+    # plt.show()
